8e8e13db27c17bd64bccdf58a3f890120f6657b4
liangjisheng <1294851990@qq.com>
2018-12-25 00:48:28 +0800

    learn-vim

---
+++ test.c
@@ -0,0 +1,19 @@
+/*Positive thinking. I’m obviously a big proponent of positive 
+thinking as the best way to achieve your goals, but it 
+to happiness too. Optimism and self-este
+are some of the best indicators of people who lead happy lives
+Happy people feel empowered, in control of their lives, and have 
+thinking as the best way to achieve yyourour goals, but it    
+you develop. Get into the habit of squashing all negative thoughts 
+positive outlook on life. Action steps: Make positive thinking 
+fact, this should be one of the first habits 
+you develop. Get into the habit of squashing all negative thoughts 
+and replacing them with positive ones. Instead of “I can’t” think
+“I can”. It may sound corny, but it has worked for me, every time*/
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", "ljs");
+	return 0;
+}
